39693b229122393f4af24115041ea26bc2eee5c9
Tacohontas <Jonathan.undhagen@gmail.com>
2020-04-23 10:32:22 +0200

    add Switch

--- src/components/Approute.js
+++ src/components/Approute.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import myBookings from "../pages/myBookings";
 import App from "./App";
 import Navbar from "./Navbar";
@@ -10,10 +10,12 @@ const Approute = () => {
   return (
     <div>
       <BrowserRouter>
-      <Navbar />
-        <Route exact path="/" component={App}></Route>
-        <Route exact path="/Bookings" component={myBookings}></Route>
-        <Route component={NotFound}></Route>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={App}></Route>
+          <Route exact path="/Bookings" component={myBookings}></Route>
+          <Route component={NotFound}></Route>
+        </Switch>
       </BrowserRouter>
     </div>
   );
